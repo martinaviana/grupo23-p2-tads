5aacec9e6654e8f2e65c80921de94de9dd3db15e
unknown <mviana2@correo.um.edu.uy>
2023-06-06 19:33:17 -0300

    tests

--- test/uy/edu/um/prog2/tad/heap/MyHeapImplTest.java
+++ test/uy/edu/um/prog2/tad/heap/MyHeapImplTest.java
@@ -1,4 +1,21 @@
+package uy.edu.um.prog2.tad.heap;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MyHeapImplTest {
-  
+
+    @Test
+    void insert() {
+        //MyHeap<Integer, String> heap = new MyHeapImpl<>();
+    }
+
+    @Test
+    void deleteAndReturn() {
+    }
+
+    @Test
+    void size() {
+    }
 }
