a4807070ed3161d0cd8c39f6c0d2a763e2b0bdc8
Santiago-Per <spereira6@correo.um.edu.uy>
2023-06-06 21:12:41 -0300

    Heap test

---
+++ test/uy/edu/um/prog2/tad/hash/MyHashImplTest.java
@@ -0,0 +1,54 @@
+package uy.edu.um.prog2.tad.hash;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import uy.edu.um.prog2.tad.hash.ClosedHashImpl;
+import uy.edu.um.prog2.tad.hash.MyHash;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+public class MyHashImplTest {
+
+    @Test
+    void put() {
+        MyHash<Integer,Integer> hash = new ClosedHashImpl<>(11,true);
+        hash.put(1, 1111);
+        hash.put(2, 549);
+        hash.put(3, 158);
+
+
+        assertEquals(549, hash.get(2));
+    }
+
+    @Test
+    void remove() {
+        MyHash<Integer, Integer> hash = new ClosedHashImpl<>(11, true);
+
+        hash.put(1,1111);
+        hash.put(2,549);
+        hash.put(3,158);
+        hash.put(4,5678);
+
+        hash.remove(3);
+
+        assertNull(hash.get(3));
+    }
+
+
+    @Test
+    void get() {
+        MyHash<Integer,Integer> hash = new ClosedHashImpl<>(11,true);
+
+        hash.put(1,1111);
+        hash.put(2,549);
+        hash.put(3,158);
+        hash.put(4,5678);
+        hash.put(3,510);
+        hash.put(5, 1000);
+
+        assertEquals(549,hash.get(2));
+        assertEquals(1000, hash.get(5));
+    }
+}
