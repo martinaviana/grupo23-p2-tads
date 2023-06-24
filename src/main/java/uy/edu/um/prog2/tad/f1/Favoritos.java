c062924905c66535468ce3c994d6d2c20a15aa95
unknown <mviana2@correo.um.edu.uy>
2023-06-24 17:14:01 -0300

    consulta 5

--- src/main/java/uy/edu/um/prog2/tad/f1/Favoritos.java
+++ src/main/java/uy/edu/um/prog2/tad/f1/Favoritos.java
@@ -14,7 +14,6 @@ public class Favoritos implements TweetRecordCallback {
 
     private MyLinkedListImpl<Node<Integer, String>> result = new MyLinkedListImpl<>();
 
-    private MyBinarySearchTreeImpl<Integer ,String> arbolFavoritos= new MyBinarySearchTreeImpl<>();
 
     @Override
     public void execute(CSVRecord record) {
@@ -22,33 +21,42 @@ public class Favoritos implements TweetRecordCallback {
             String favourites = record.get("user_favourites");
             double parseFavourites = Double.parseDouble(favourites);
             int parseFavouritesInt = (int) Math.round(parseFavourites);
-            arbolFavoritos.add(parseFavouritesInt, record.get("user_name"));
-            aux();
-        }catch (NumberFormatException e) {
-
-        }
-    }
-    private void calculate7Most(Node<Integer, String> node) {
-
-        if (node.getRight() != null) {
-            calculate7Most(node.getRight());
-        }
-        if (result.size() == 7)
-            return;
-        if (!result.contains(node))
+            Node<Integer, String> node = new Node(parseFavouritesInt, record.get("user_name"));
             result.add(node);
+            calculate7Most();
+        } catch (NumberFormatException e) {
 
-        if (node.getLeft() != null) {
-            calculate7Most(node.getLeft());
-        } else {
-            return;
         }
     }
-    public void aux(){
-        calculate7Most(arbolFavoritos.getRoot());
+
+    private void calculate7Most() {
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = 0; j < result.size() - i - 1; j++) {
+                try {
+                    if (result.get(j).getKey() < result.get(j + 1).getKey()) {
+                        Node<Integer, String> temp = result.get(j);
+                        Node<Integer, String> temp2 = result.get(j + 1);
+                        result.get(j).setKey(temp2.getKey());
+                        result.get(j).setValue(temp2.getValue());
+                        result.get(j + 1).setKey(temp.getKey());
+                        result.get(j + 1).setValue(temp.getValue());
+
+                    }
+                } catch (EmptyListException e) {
+                    throw new RuntimeException(e);
+                } catch (OutOfBoundsException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
-    public MyLinkedListImpl<Node<Integer, String>> getFavoritos(){
+
+    public MyLinkedListImpl<Node<Integer, String>> getFavoritos() {
         return result;
     }
 }
+
+
+
+
 
